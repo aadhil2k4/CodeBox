aad96ec40fc5f9c2c7c9ccad28b5298fb2b15889
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-20 18:45:11 +0530

    added user authentication router and middleware

---
+++ backend/Router/AuthRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/login', (req,res)=>{
+    res.send('Login Success');
+})
+
+router.post('/signup', (req,res)=>{
+    res.send('SignUp Success');
+})
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ require('./Models/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const authRouter = require('./Router/AuthRouter');
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
@@ -13,7 +15,7 @@ app.get('/', (req,res)=>{
 
 app.use(bodyParser.json());
 app.use(cors());
-//app.use('/auth', )
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
