df1965d5d2be705858d0695732179a1e4bafdcef
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-20 18:22:58 +0530

    Defined User schema in mongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 require('./Models/db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
     res.send('Hi');
 })
+
+app.use(bodyParser.json());
+app.use(cors());
+//app.use('/auth', )
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
