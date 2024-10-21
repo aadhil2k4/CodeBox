f2c22a4a2e53d7ea2078466a547a057b6600cc4f
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-10-21 17:02:17 +0530

    minor fixes in router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import Home from './components/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
          
         </Routes>
-      </BrowserRouter>
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 const theme = createTheme({
   typography:{
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+     <BrowserRouter> 
     <App />
+    </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
