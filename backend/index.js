0383a7060f91838d5cc14834fbd355a45ac94bb7
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-11-25 20:05:00 +0530

    backend for terminal ✅

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,38 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http')
 const {Server: SocketServer } = require('socket.io');
+const pty = require("node-pty");
+
+const ptyProcess = pty.spawn('bash', [], {
+    name: 'xtern-color',
+    cols: 80,
+    rows:30,
+    cwd: process.env.INIT_CWD,
+    env: process.env
+}) 
 
 const server = http.createServer(app);
 const io = new SocketServer({
-    cors: '*'
+    cors: {origin: '*'}
 })
+
 io.attach(server);
-io.on('Connection', (socket)=>{
+
+ptyProcess.onData(data => {
+    io.emit('terminal:data', data);
+})
+
+io.on('connection', (socket)=>{
     console.log(`Socket connected`, socket.id);
+    socket.on('terminal:write', (data)=>{
+        ptyProcess.write(data);
+    })
 })
 
 const authRouter = require('./Router/AuthRouter');
 
 const PORT = process.env.PORT || 3000;
+const DPORT = 9000
 
 app.get('/', (req,res)=>{
     res.send('Hi');
@@ -28,7 +47,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth', authRouter);
 
-server.listen(9000, ()=>console.log(`🐳 Docker server runninig on port 9000`))
+server.listen(DPORT, ()=>console.log(`🐳 Docker server runninig on port ${DPORT}`))
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
